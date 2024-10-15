fb87c85411db899252ec175a9e35a329a54549de
MR LUTHER <lutherofficial009@gmail.com>
2024-10-15 16:35:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ global.timezone= process.env.TZ || process.env.TIME_ZONE || "Asia/kalkata";
 global.github=process.env.GITHUB|| "https://github.com/SuhailTechInfo/Suhail-Md";
 global.gurl  =process.env.GURL  || "https://whatsapp.com/channel/0029Vakxzdf7oQhll9dE2S0t";
 global.website=process.env.GURL || "https://whatsapp.com/channel/0029Vakxzdf7oQhll9dE2S0t" ; 
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/d5b1c3544fedc23e11a06.jpg" ; // SET LOGO FOR IMAGE 
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://i.imgur.com/CadgoD3.jpeg" ; // SET LOGO FOR IMAGE 
 
 
 
